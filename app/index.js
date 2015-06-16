de46eb87ab9724bb3541c7e840ef61b5960b1fc0
David Soff <david@soff.nl>
2015-06-16 23:51:29 +0200

    fixed #1 and #2

--- app/index.js
+++ app/index.js
@@ -1,36 +1,53 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
 
 
-var FreshGenerator = module.exports = function FreshGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+	this.on('end', function () {
+	  this.installDependencies({ 
+	  	skipInstall: true,
+	  	skipMessage: true
+	  });
+	});
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: true });
-  });
+    // This makes `appname` a required argument.
+    this.argument('dirname', { type: String, required: false });
+  },
+  prompting: function(){
+  	if(!this.dirname){
+  		var done = this.async();
+	    this.prompt({
+	      type    : 'input',
+	      name    : 'name',
+	      message : 'Your project name',
+	      default : this.appname // Default to current folder name
+	    }, function (answers) {
+	      this.dirname = answers.name;
+	      done();
+	    }.bind(this));
+  	}
+  },
+  writing: function () {
+	this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true }).forEach(function (file) {
+	  if ([
+	    '.git'
+	  ].indexOf(file) !== -1) {
+	    return;
+	  }
+	  this.copy(file, this.dirname+'/'+file);
+	}, this);
+  },
+  end: function(){
+  	this.log(yosay(
+        chalk.red('Welcome!') + '\n' +
+        chalk.yellow('Start your fresh meteor app by running:\n cd '+this.dirname+' && meteor')
+      ));
+  }
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-};
 
-util.inherits(FreshGenerator, yeoman.generators.Base);
-
-FreshGenerator.prototype.askFor = function askFor() {
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
-};
-
-FreshGenerator.prototype.app = function app() {
-  this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true }).forEach(function (file) {
-    if ([
-      '.git'
-    ].indexOf(file) !== -1) {
-      return;
-    }
-    this.copy(file, file);
-  }, this);
-};
-
-FreshGenerator.prototype.projectfiles = function projectfiles() {
-};
+})
