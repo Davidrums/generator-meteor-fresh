6d93aedae3007ad588d174a6113df22707eb58af
David Soff <david@soff.nl>
2015-06-16 21:11:43 +0200

    finished generator

--- app/index.js
+++ app/index.js
@@ -1,26 +1,36 @@
-var fs = require('fs');
+'use strict';
+var util = require('util');
 var path = require('path');
+var yeoman = require('yeoman-generator');
 
-var Generator = module.exports = function() {
-  var prompts = [];
-  var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
-  var ignores = [
-    '.git',
-    'LICENSE',
-    'README.md',
-  ];
 
-  this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+var FreshGenerator = module.exports = function FreshGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
 
-  this.log.writeln('Generating from ' + 'Generator Boilerplate'.cyan + ' v' + this.package.version.cyan + '...');
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: true });
+  });
 
-  files.forEach(function(file) {
-    if (ignores.indexOf(file) !== -1) {
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(FreshGenerator, yeoman.generators.Base);
+
+FreshGenerator.prototype.askFor = function askFor() {
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+};
+
+FreshGenerator.prototype.app = function app() {
+  this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true }).forEach(function (file) {
+    if ([
+      '.git'
+    ].indexOf(file) !== -1) {
       return;
     }
-
     this.copy(file, file);
   }, this);
 };
 
-Generator.name = "Generator Boilerplate";
+FreshGenerator.prototype.projectfiles = function projectfiles() {
+};
